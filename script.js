38bcb2b6b32d226897737f1dd267bb389e7a5af7
Adam (Futur3Sn0w) <futur3sn0w@gmail.com>
2022-01-15 14:44:32 -0500

    Weather + Time&Date

--- script.js
+++ script.js
@@ -42,7 +42,8 @@ function finishBtn(obj) {
     mainTitle.innerHTML = overviewTB.value;
     mainStatus.innerHTML = statusTB.value;
     cont.style.backgroundColor = localStorage.getItem('colorMainLS');
-    swiper.style.background = "linear-gradient(90deg, " + localStorage.getItem('colorSwiperLS') + ", transparent)";
+    swiper.style.background = "linear-gradient(90deg, transparent, " + localStorage.getItem('colorSwiperLS') + ")";
+    document.documentElement.style.setProperty('--textClr', localStorage.getItem("textColor"));
 
     localStorage.setItem('titleLS', overviewTB.value);
     localStorage.setItem('statusLS', statusTB.value);
@@ -55,10 +56,85 @@ window.onload = function () {
     mainTitle.innerHTML = localStorage.getItem('titleLS');
     mainStatus.innerHTML = localStorage.getItem('statusLS');
     cont.style.backgroundColor = localStorage.getItem('colorMainLS');
-    swiper.style.background = "linear-gradient(90deg, " + localStorage.getItem('colorSwiperLS') + ", transparent)";
+    swiper.style.background = "linear-gradient(90deg, transparent, " + localStorage.getItem('colorSwiperLS') + ")";
+    document.documentElement.style.setProperty('--textClr', localStorage.getItem("textColor"));
 }
 
 function globalColor(obj2) {
     localStorage.setItem("colorMainLS", "var(--" + obj2.getAttribute('data-colorName') + ")")
     localStorage.setItem("colorSwiperLS", obj2.getAttribute('data-colorName'))
+
+    if (obj2.getAttribute('data-colorName') === "orange"){
+        localStorage.setItem("textColor", "black");
+    } else if (obj2.getAttribute('data-colorName') === "yellow") {
+        localStorage.setItem("textColor", "black");
+    } else if (obj2.getAttribute('data-colorName') === "green") {
+        localStorage.setItem("textColor", "black");
+    } else {
+        localStorage.setItem("textColor", "white");
+    }
 }
+
+setInterval(showTime, 1000);
+function showTime() {
+    let time = new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    am_pm = "AM";
+
+    var dd = String(time.getDate()).padStart(2, '0');
+    var mm = String(time.getMonth() + 1).padStart(2, '0');
+  
+    if (hour > 12) {
+        hour -= 12;
+        am_pm = " PM";
+    }
+    if (hour == 0) {
+        hr = 12;
+        am_pm = " AM";
+    }
+  
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+  
+    let currentTime = hour + ":" + min + am_pm;
+    let currentDate = mm + "/" + dd;
+  
+    document.getElementById("time").innerHTML = currentTime;
+    // document.getElementById("date").innerHTML = currentDate;
+}
+showTime();
+
+var objToday = new Date(),
+	weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
+	dayOfWeek = weekday[objToday.getDay()],
+	domEnder = function() { var a = objToday; if (/1/.test(parseInt((a + "").charAt(0)))) return "th"; a = parseInt((a + "").charAt(1)); return 1 == a ? "st" : 2 == a ? "nd" : 3 == a ? "rd" : "th" }(),
+	dayOfMonth = todayDate + ( objToday.getDate() < 10) ? '0' + objToday.getDate() + domEnder : objToday.getDate() + domEnder,
+	months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
+	curMonth = months[objToday.getMonth()],
+	curYear = objToday.getFullYear();
+var todayDate = dayOfWeek + ", " + curMonth + " " + dayOfMonth;
+
+document.getElementById('date').innerHTML = todayDate;
+
+function weatherBalloon( cityID ) {
+	var key = '984b3d6c3b801e60a2eaf094da08b866';
+	fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
+	.then(function(resp) { return resp.json() }) // Convert data to json
+	.then(function(data) {
+		drawWeather(data); // Call drawWeather
+	})
+	.catch(function() {
+		// catch any errors
+	});
+}
+
+function drawWeather( d ) {
+	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
+	
+	document.getElementById('weather').innerHTML = fahrenheit + '&deg | ' + d.weather[0].description;
+	// document.getElementById('temp').innerHTML = fahrenheit + '&deg;';
+	// document.getElementById('location').innerHTML = d.name;
+}
+
+weatherBalloon( 4791259 );
